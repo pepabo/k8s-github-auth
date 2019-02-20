88809632aa5317928b93b888371e2a1b6acd2cc8
r_takaishi <ryo.takaishi.0@gmail.com>
2019-02-20 18:19:56 +0900

    環境変数がセットされている場合優先

--- main.go
+++ main.go
@@ -19,11 +19,27 @@ func main() {
 			Name: "github-upload-url",
 		},
 		cli.StringFlag{
-			Name: "team",
+			Name: "organization",
 		},
 	}
 	app.Action = func(c *cli.Context) error {
-		return server.Start(c.String("github-base-url"), c.String("github-upload-url"), c.String("team"))
+		baseUrl := c.String("github-base-url")
+		uploadUrl := c.String("github-upload-url")
+		team := c.String("team")
+
+		if os.Getenv("GITHUB_BASE_URL") != "" {
+			baseUrl = os.Getenv("GITHUB_BASE_URL")
+		}
+
+		if os.Getenv("GITHUB_UPLOAD_URL") != "" {
+			uploadUrl = os.Getenv("GITHUB_UPLOAD_URL")
+		}
+
+		if os.Getenv("ORGANIZATION") != "" {
+			baseUrl = os.Getenv("ORGANIZATION")
+		}
+
+		return server.Start(baseUrl, uploadUrl, team)
 	}
 
 	err := app.Run(os.Args)
