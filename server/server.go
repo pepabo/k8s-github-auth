e80758bf717b828019949df9a0f28038c1324b41
r_takaishi <ryo.takaishi.0@gmail.com>
2019-12-27 15:39:19 +0900

    [refactor] move structs to types.go

--- server/server.go
+++ server/server.go
@@ -72,35 +72,6 @@ func Start(baseUrl string, uploadUrl string, org string) error {
 	return nil
 }
 
-type AuthenticationRequest struct {
-	ApiVersion string                 `json:"apiVersion"`
-	Kind       string                 `json:"kind"`
-	Metadata   map[string]interface{} `json:"metadata"`
-	Spec       struct {
-		Token string `json:"token"`
-	}
-	Status struct {
-		User map[string]interface{} `json:"user"`
-	}
-}
-
-type AuthenticationResponse struct {
-	ApiVersion string `json:"apiVersion"`
-	Kind       string `json:"kind"`
-	Status     Status `json:"status"`
-}
-
-type Status struct {
-	Authenticated bool `json:"authenticated"`
-	User          User `json:"user"`
-}
-
-type User struct {
-	Groups   []string `json:"groups"`
-	UID      string   `json:"uid"`
-	Username string   `json:"username"`
-}
-
 func getUserInfo(github_base_url string, token string) (github.User, error) {
 	var u github.User
 	req, _ := http.NewRequest("GET", github_base_url+"/user", nil)
